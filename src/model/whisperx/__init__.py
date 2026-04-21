6b3da66a0c6c304fdef161311509ae380a864086
synthereal <infernofire027@gmail.com>
2026-04-21 04:16:51 -0700

    logger fix

--- src/model/whisperx/__init__.py
+++ src/model/whisperx/__init__.py
@@ -16,6 +16,12 @@ def _setup_warnings():
 
 _setup_warnings()
 logger = logging.getLogger(__name__)
+if not logger.handlers:
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(name)s - %(message)s"))
+    logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+logger.propagate = False
 
 import os
 import gc
